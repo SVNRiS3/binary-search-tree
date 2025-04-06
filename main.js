c5907084256cd275c7fe04eab6b0306ce0763387
Chris <krzy.driwa@gmail.com>
2025-04-06 10:54:05 +0200

    Sort and remove duplicates from an array

--- main.js
+++ main.js
@@ -14,7 +14,10 @@ class Tree {
   }
 }
 
-function buildTree(arr) {}
+function buildTree(arr) {
+  const sortedArr = mergeSort(arr);
+  const SortedArrUniq = [...new Set(sortedArr)];
+}
 
 const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
