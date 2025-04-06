182fd4645b3b4faddddc9c4be69f125223fd5766
Chris <krzy.driwa@gmail.com>
2025-04-06 10:38:18 +0200

    Modify sorting to remove duplicate values

--- merge-sort.js
+++ merge-sort.js
@@ -6,7 +6,10 @@ export default function mergeSort(arr, sortedArr = []) {
   //recursive case
   //for item half of the array
   //push a if a< b else push b, where b is other half of the array
-  if (left > right) {
+  if (left === right) {
+    sortedArr.push(right.shift());
+    left.shift();
+  } else if (left > right) {
     sortedArr.push(right.shift());
     sortedArr.push(left.shift());
   } else {
