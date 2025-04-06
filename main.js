c282e4c9013d998ee664f089987ae8746d187f6c
Chris <krzy.driwa@gmail.com>
2025-04-06 10:29:52 +0200

    Prepare classes and function needed for BST

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ class Node {
     this.right = right;
   }
 }
+
+class Tree {
+  constructor(arr) {
+    this.root = buildTree(arr);
+  }
+}
+
+function buildTree(arr) {
+  //TODO
+}
+
+const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const BST = new Tree(buildTree(testArr));
